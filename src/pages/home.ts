8b2bbcb291563989279a26d62992250c5005da7a
Sylvain Girard <sylvain97@gmail.com>
2024-07-15 15:34:02 +1000

    Make hero quicklinks only animate on tablet and above

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -5,18 +5,25 @@ window.addEventListener(SCRIPTS_LOADED_EVENT, () => {
 });
 
 function quicklinkCards() {
-  window.gsap.to('.home_quicklinks-card-link', {
-    y: '0%',
-    duration: 0.5,
-    stagger: {
-      each: 0.2,
-    },
-    //ease: "power4.out",
-    scrollTrigger: {
-      trigger: '.home_quicklinks-wrapper',
-      start: 'bottom bottom',
-      toggleActions: 'play none none none',
-      //markers: true,
-    },
+  // create
+  let mm = gsap.matchMedia();
+
+  mm.add('(min-width: 800px)', () => {
+    window.gsap.to('.home_quicklinks-card-link', {
+      y: '0%',
+      duration: 0.5,
+      stagger: {
+        each: 0.2,
+      },
+      //ease: "power4.out",
+      scrollTrigger: {
+        trigger: '.home_quicklinks-wrapper',
+        start: 'bottom bottom',
+        toggleActions: 'play none none none',
+        //markers: true,
+      },
+    });
   });
+  // later, if we need to revert all the animations/ScrollTriggers...
+  mm.revert();
 }

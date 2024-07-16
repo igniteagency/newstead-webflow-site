d86f20cbeab4f62a1362f1c4059cc2eeeea77267
Sylvain Girard <sylvain97@gmail.com>
2024-07-16 11:18:35 +1000

    Adjust rotation on CTA mobile animation

--- src/components/cta.ts
+++ src/components/cta.ts
@@ -1,5 +1,5 @@
 export function ctaAnimation() {
-  let mm = gsap.matchMedia();
+  let mm = window.gsap.matchMedia();
   const breakPoint = 991; // Define the breakpoint for mobile/desktop
 
   mm.add(
@@ -7,20 +7,40 @@ export function ctaAnimation() {
       isDesktop: `(min-width: ${breakPoint}px)`,
       isMobile: `(max-width: ${breakPoint - 1}px)`,
     },
-    (context) => {
-      let isDesktop = context.conditions;
+    (context: gsap.Context) => {
+      let { isDesktop, isMobile } = context.conditions || {};
 
-      gsap.from('.cta_graphic-wrapper, .cta_shape', {
-        rotationZ: isDesktop ? -25 : 60, // Use -25 for desktop, 60 for mobile
-        duration: 1.5,
-        ease: 'power4.inOut',
-        scrollTrigger: {
-          trigger: '.section_cta',
-          start: 'top 80%',
-          end: 'top top',
-          toggleActions: 'play none none none',
-        },
-      });
+      if (isDesktop) {
+        // Desktop animation
+        window.gsap.from('.cta_graphic-wrapper, .cta_shape', {
+          rotationZ: -25,
+          duration: 1.5,
+          ease: 'power4.inOut',
+          scrollTrigger: {
+            trigger: '.section_cta',
+            start: 'top 80%',
+            end: 'top top',
+            toggleActions: 'play none none none',
+          },
+        });
+      }
+
+      if (isMobile) {
+        console.log('isMobile');
+        // Mobile animation
+        window.gsap.set('.cta_graphic-wrapper', { rotationZ: 0 });
+        window.gsap.from('.cta-graphic, .cta_shape', {
+          rotationZ: 70,
+          duration: 1.5,
+          ease: 'power4.inOut',
+          scrollTrigger: {
+            trigger: '.section_cta',
+            start: 'top 80%',
+            end: 'top top',
+            toggleActions: 'play none none none',
+          },
+        });
+      }
 
       return () => {
         // Cleanup function if needed
